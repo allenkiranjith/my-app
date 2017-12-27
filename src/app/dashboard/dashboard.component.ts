56d002c670c68da5c2c91caaf6d185a5c25a918f
allenkiranjith <ranjith.allenki@gmail.com>
2017-12-26 19:17:04 -0800

    added json file and reading data from json

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { IUserData } from '../shared/models/IUserData';
+import { DashboardService } from './dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
+  providers: [ DashboardService ]
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  data: IUserData[];
+
+  constructor(private dashboardService: DashboardService) { }
 
   ngOnInit() {
+    this.dashboardService.getData().subscribe(results => {
+      this.data = <IUserData[]> results;
+      console.log(this.data);
+    });
   }
 
 }
---
+++ src/app/shared/models/IUserData.ts
@@ -0,0 +1,5 @@
+export interface IUserData {
+    name?: string;
+    category?: string;
+    amount?: number;
+}
